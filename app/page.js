c764c1c91b0e998355ab8348c03816ba43cdc623
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-18 19:40:15 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,20 +7,30 @@ function Home() {
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
   const [lapVisible, setLapVisible] = useState(false);
+  const [timerRunning, setTimerRunning] = useState(false);
 
   const startTimer = () => {
-    startTimeRef.current = Date.now() - currentTime;
-    intervalRef.current = setInterval(updateTime, 10);
+    if (!timerRunning) {
+      startTimeRef.current = Date.now() - currentTime;
+      intervalRef.current = setInterval(updateTime, 10);
+      setTimerRunning(true);
+    }
   };
 
   const stopTimer = () => {
-    clearInterval(intervalRef.current);
-    intervalRef.current = null;
+    if (timerRunning) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+      setTimerRunning(false);
+    }
   };
 
   const lapTimer = () => {
-    const newLapTime = currentTime;
-    setLaps((prevLaps) => [...prevLaps, newLapTime]);
+    if (timerRunning) {
+      const newLapTime = currentTime;
+      setLaps((prevLaps) => [...prevLaps, newLapTime]);
+      setLapVisible(true);
+    }
   };
 
   const resetTimer = () => {
@@ -28,6 +38,7 @@ function Home() {
     setCurrentTime(0);
     setLaps([]);
     setLapVisible(false);
+    setTimerRunning(false);
   };
 
   const updateTime = () => {
@@ -40,15 +51,19 @@ function Home() {
     }
   }, []);
 
+  const formatTime = (time) => {
+    if (time === 0) return '';
+    const seconds = Math.floor(time / 1000);
+    const milliseconds = (time % 1000).toString().padStart(3, '0');
+    return `${seconds}.${milliseconds}`;
+  };
+
   return (
     <div id="main">
       <section>
         <h1 className="seconds-elapsed">Stopwatch Time</h1>
         <p className="stopwatch-time">
-          {currentTime / 1000.0}
-          <span className="decimal-places">
-            {currentTime % 1000 / 1000.0}
-          </span>
+          {formatTime(currentTime)}
         </p>
         <section className="buttons">
           <button className="start-btn" onClick={startTimer}>
@@ -70,7 +85,7 @@ function Home() {
           <h2>Laps</h2>
           <section className="laps">
             {laps.map((lapTime, index) => (
-              <p key={index}>Lap {index + 1}: {lapTime / 1000.0}</p>
+              <p key={index}>Lap {index + 1}: {formatTime(lapTime)}</p>
             ))}
           </section>
         </section>
